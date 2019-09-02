7acd0fb68c4b72bf44406b6a1aaad84e276bfce0
stephenwhippuk <54818730+stephenwhippuk@users.noreply.github.com>
2019-09-02 21:07:20 +0100

    Add files via upload

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace MyJukeBox.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
